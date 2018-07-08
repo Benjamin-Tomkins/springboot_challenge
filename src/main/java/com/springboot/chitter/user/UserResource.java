a7597664597a2a96466b9c6281cf69428a48caf0
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-08 12:21:52 +0100

    Added customised error responses and unified all responses to a custom format

--- src/main/java/com/springboot/chitter/user/UserResource.java
+++ src/main/java/com/springboot/chitter/user/UserResource.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
