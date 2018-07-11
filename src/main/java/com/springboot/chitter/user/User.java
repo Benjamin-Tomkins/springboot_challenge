29e40fae71b42ad92af099f2ce06b57d0ad7f087
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-11 07:19:24 +0100

    Added test data

--- src/main/java/com/springboot/chitter/user/User.java
+++ src/main/java/com/springboot/chitter/user/User.java
@@ -23,11 +23,8 @@ public class User {
     @GeneratedValue
     private Integer id;
 
-    @Size(min=2, message="Name should have at least 2 characters")
-    private String name;
-
-    @Past
-    private Date birthDate;
+    @Size(min=2, message="Email should have at least 2 characters")
+    private String email;
 
 
     @OneToMany(mappedBy="user")
@@ -41,11 +38,10 @@ public class User {
 
     protected User() {}
 
-    public User(Integer id, String name, Date birthDate) {
+    public User(Integer id, String email) {
         super();
         this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
+        this.email = email;
     }
 
 
@@ -57,21 +53,14 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getEmail() {
+        return email;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
 
     public List<Post> getPosts() {
         return posts;
@@ -83,7 +72,7 @@ public class User {
 
     @Override
     public String toString() {
-        return String.format("User [id=%s, name=%s, birthDate=%s]", id, name, birthDate);
+        return String.format("User [id=%s, email=%s]", id, email);
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,24 @@
-INSERT INTO user VALUES(1001, sysdate(), 'AB');
-INSERT INTO user VALUES(1002, sysdate(), 'Jack');
-INSERT INTO user VALUES(1003, sysdate(), 'Jill');
+INSERT INTO user(id,email)
+VALUES(1001, 'ben@spring.com');
+INSERT INTO user(id,email)
+VALUES(1002, 'dan@spring.com');
+INSERT INTO user(id,email)
+VALUES(1003, 'rahul@spring.com');
 
-INSERT INTO post VALUES(1101, 'AB''s first post', 1001);
-INSERT INTO post VALUES(1102, 'AB''s second post', 1001);
+INSERT INTO post(id,post_text,user_id)
+VALUES(2001, 'ben''s first post', 1001);
+INSERT INTO post(id,post_text,user_id)
+VALUES(2002, 'ben''s second post', 1001);
+INSERT INTO post(id,post_text,user_id)
+VALUES(2003, 'dan''s first post', 1002);
 
+INSERT INTO comment(id,comment_text,post_id,user_id)
+VALUES(3001, 'dan hates ben''s post', 2001, 1002);
+INSERT INTO comment(id,comment_text,post_id,user_id)
+VALUES(3002, 'ben replies to den''s mean post and is very cordial', 2001, 1001);
+INSERT INTO comment(id,comment_text,post_id,user_id)
+VALUES(3003, 'rahul hates on brew', 2001, 1003);
+INSERT INTO comment(id,comment_text,post_id,user_id)
+VALUES(3004, 'ben tells dan a joke about cows', 2003, 1001);
+INSERT INTO comment(id,comment_text,post_id,user_id)
+VALUES(3005, 'dan chortles', 2003, 1002);
